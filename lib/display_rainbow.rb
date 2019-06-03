080c6c92da3a94196eb676ca8897ea235b77d2df
Tyson Weatherly <tysonw1612@gmail.com>
2019-06-03 21:45:55 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,8 +2,8 @@
 
 an_array_of_colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
-def display_rainbow ()
+def display_rainbow (an_array_of_colors)
   puts "R: #{0}, O: #{1}, Y: #{2}, G: #{3}, B: #{4}, I: #{5}, V: #{6}"
 end
 
-display_rainbow(an_array_of_colors)
+display_rainbow
