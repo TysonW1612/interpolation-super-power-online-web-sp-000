355b2b02b97683fb7ba401a133dc383f7e35b181
Tyson Weatherly <tysonw1612@gmail.com>
2019-06-03 21:55:06 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,7 +3,7 @@
 an_array_of_colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 
 def display_rainbow (an_array_of_colors)
-  puts "R: #{an_array_of_colors[0]}, O: #{an_array_of_colors[1]}, Y: #{an_array of _colors[2]}, G: #{an_array_of_colors[3]}, B: #{an_array_of_colors[4]}, I: #{an_array_of_colors[5]}, V: #{an_array_of_colors[6]}"
+  print "R: #{an_array_of_colors[0]}, O: #{an_array_of_colors[1]}, Y: #{an_array of _colors[2]}, G: #{an_array_of_colors[3]}, B: #{an_array_of_colors[4]}, I: #{an_array_of_colors[5]}, V: #{an_array_of_colors[6]}"
 end
 
 
